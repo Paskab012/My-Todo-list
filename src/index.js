682ea45d15f8899cdcc02d795e01af4e572fec0f
KABIKA681 <pascalkabikampan@gmail.com>
2022-02-25 14:13:14 +0200

    Structure basically mytodo project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import './style.css';
 
-import { camelCase } from 'lodash';
+let tasks = [];
 
-console.log(camelCase('hello world'));
+const addToLocalStorage = () => {
+  localStorage.setItem('myTasks', JSON.stringify(tasks));
+};
+
+const getFromLocalStorage = () => {
+  if (localStorage.getItem('myTasks')) {
+    tasks = JSON.parse(localStorage.getItem('myTasks'));
+  }
+  return tasks;
+};
+
+const resetIndex = (tasks) => {
+  for (let i = 0; i < tasks.length; i += 1) {
+    let index = i + 1;
+    index = i;
+    tasks[i].index = index;
+  }
+};
